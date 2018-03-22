ebc7a28706d5785d5b4baf3e7d61e97ed02ec2ae
Luiz Keese Tabacow <keese.luiz@gmail.com>
2018-03-22 20:49:55 -0300

    Added describe to tests

--- nodeTests/server/server.test.js
+++ nodeTests/server/server.test.js
@@ -3,23 +3,29 @@ const expect = require('expect');
 
 var app = require('./server').app;
 
-it('should return hello world response', (done) => {
-    request(app)
-        .get('/')
-        .expect(200)
-        .expect('Hello World!')
-        .end(done);
-});
+describe('Server', () => {
+    describe('GET /', () => {
+        it('should return hello world response', (done) => {
+            request(app)
+                .get('/')
+                .expect(200)
+                .expect('Hello World!')
+                .end(done);
+        });
+    });
 
-it('should return my user', (done) => {
-    request(app)
-        .get('/users')
-        .expect(200)
-        .expect((response) => {
-            expect(response.body).toInclude({
-                                                name: 'Luiz',
-                                                age: 24
-                                            });
-        })
-        .end(done);
+    describe('GET /users', () => {
+        it('should return my user', (done) => {
+            request(app)
+                .get('/users')
+                .expect(200)
+                .expect((response) => {
+                    expect(response.body).toInclude({
+                                                        name: 'Luiz',
+                                                        age: 24
+                                                    });
+                })
+                .end(done);
+        });
+    });
 });
